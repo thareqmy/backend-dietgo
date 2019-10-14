a3dca7b99b9dc21c1b0ecf2f42532031a83ca5ee
Thareq Yusuf <thareqmyha@gmail.com>
2019-10-14 22:49:19 +0700

    add models

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, jsonify, request, render_template, json
 from dotenv import load_dotenv
-
+from keras.models import model_from_json
 
 app = Flask(__name__)
 load_dotenv()
@@ -17,6 +17,24 @@ def get_username():
         id=1234
     )
     return data
+
+@app.route('/predict', methods=['GET','POST'])
+def predict_food():
+
+    # Model reconstruction from JSON file
+    with open('model/vgg16.json', 'r') as f:
+        model = model_from_json(f.read())
+
+    # Load weights into the new model
+    model.load_weights('model/vgg16.h5')
+    if request.files:
+        image = request.files["image"]
+        IMAGE_SIZE = 200
+        image = np.expand_dims(image, axis=0)
+        result = model.predict(image)[0]
+        return jsonify(result)
+
+
 
 if __name__ == '__main__':
     import os
